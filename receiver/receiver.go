d28b8591f31b079baa534c96b62eaa0fd44f6313
Soumik Dutta <shalearkane@gmail.com>
2024-04-19 14:29:16 +0530

    simplify sending messages

--- receiver/receiver.go
+++ receiver/receiver.go
@@ -3,27 +3,20 @@ package receiver
 import (
 	"context"
 	"fmt"
-	"sync"
 )
 
 type Receiver struct {
-	In   *chan int
-	Jobs chan int
+	In *chan int
 }
 
-func (c Receiver) Work(wg *sync.WaitGroup) {
-	defer wg.Done()
-	for job := range c.Jobs {
-		fmt.Printf("%dth job finished\n", job)
-	}
-}
 func (c Receiver) Receive(ctx context.Context) {
 	for {
 		select {
 		case job := <-*c.In:
-			c.Jobs <- job
+			{
+				println(fmt.Sprintf("Got a job %d", job))
+			}
 		case <-ctx.Done():
-			close(c.Jobs)
 			return
 		}
 	}
